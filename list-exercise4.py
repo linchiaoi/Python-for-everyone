7d2f7f0da18741cf1d67964820b878e8e1aeb444
Niklas Pragsten <niklas.pragsten@gmail.com>
2018-01-08 17:44:10 +0100

    Ex4-Added comments

--- list-exercise4.py
+++ list-exercise4.py
@@ -6,7 +6,9 @@ Created on Sun Jan  7 21:05:20 2018
 """
 new_list = []
 
-
+# The namings are really terrible, really hard to follow. 
+# Grade C- for the variable names
+# Grade A for the other stuff
 reading0 = input('Please enter a file')
 reading = open(reading0)
 
@@ -17,11 +19,12 @@ for line in reading:
         if i in new_list:
             continue
         else:
+            # To add stuff to a list you use the append function
             new_list.append(i)
 #print (new_list)
             
             
-
+# When sorting, the list doesn't return anything. It is just sorted
 new_list.sort()
 print(new_list)
 
